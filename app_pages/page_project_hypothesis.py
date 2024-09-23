72e00d48ffbc3607d3ac03639095a61187d4b577
Mattias Jörntell <mattias.jorntell@gmail.com>
2024-09-23 09:49:12 +0000

    add summary and hypothesis text

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -5,11 +5,17 @@ import matplotlib.pyplot as plt
 def page_project_hypothesis_body():
     st.write("### Project Hypothesis and Validation")
 
-    st.success(
-        f"* We suspect malaria-parasitised cells have clear marks/signs, "
-        f"typically in the middle of the cell, that can differentiate them from an uninfected cell. \n\n"
-        f"* An Image Montage shows that typically a parasitised cell has purplish marks across it. "
-        f"Average Image, Variability Image and Difference between Averages studies did not reveal "
-        f"any clear pattern to differentiate one from another."
+    st.info(
+        f"**Cherry Leaves:**\n"
+        f"* Powdery mildew infection causes white, powdery spots or patches on leaves and stems.\n"
+        f"* The powdery growth spreads, covering the entire leaf and making it appear dusted with white powder.\n"
+        f"* We believe these features are sufficient to differentiate healthy from infected leaves.\n"
+        f"* The ML model is expected to achieve at least 97% accuracy in classifying healthy and infected leaves."
+    )
 
+    st.success(
+        f"**Validation:**\n\n"
+        f"1. Visual Inspection:\n"
+        f"   - Image Montage reveals clear visual differences between healthy and infected leaves, such as white patches and color changes.\n"
+        f"* The ML pipeline performance was evaluated and it differentiates a healthy leaf and an infected leaf with at least 97% accuracy.\n\n"
     )
--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -4,32 +4,28 @@ import matplotlib.pyplot as plt
 
 def page_summary_body():
 
-    st.write("### Quick Project Summary")
+    """ Function to display project summary page """
+
+    st.title("A Brief Summary of Project")
+
+    st.header("General Information")
 
     st.info(
-        f"**General Information**\n"
-        f"* Malaria is a parasitic infection transmitted by the bite of infected female "
-        f"Anopheles mosquitoes.\n"
-        f"* A blood smear sample is collected, mixed with a reagent and examined in "
-        f"the microscope. Visual criteria are used to detect malaria parasites.\n"
-        f"* According to [WHO](https://www.who.int/news-room/fact-sheets/detail/malaria), "
-        f"in 2019, there were an estimated  229 million cases of malaria worldwide and an "
-        f"estimated 409 thousand deaths due to this disease. "
-        f"Children <5 years are the most vulnerable group, accounting for 67% (274 thousand) "
-        f"of all malaria deaths worldwide in 2019.\n\n"
-        f"**Project Dataset**\n"
-        f"* The available dataset contains 5643 out of +27 thousand images taken from "
-        f"blood smear workflow (when a drop of blood is taken on a glass slide) of "
-        f"malaria-parasitised and uninfected cells.")
+        f"**Powdery mildew** is a common fungal disease affecting cherry trees. It's caused by the pathogen Podosphaera clandestina.\n"
+        f"Infected plants exhibit white, powdery spots on their leaves and stems, often starting on the lower leaves.\n\n"
+        f"Powdery mildew is easily identifiable and can spread rapidly, causing significant damage to cherry trees.\n"
+        f"It thrives in high humidity and moderate temperatures, reducing plant yield and overall health."
+    )
 
-    st.write(
-        f"* For additional information, please visit and **read** the "
-        f"[Project README file](https://github.com/Code-Institute-Solutions/WalkthroughProject01/blob/main/README.md).")
-    
+    st.warning(
+        f"**Project Dataset**\n\n"
+        f"The dataset used in this project was sourced from Kaggle ([link](https://www.kaggle.com/codeinstitute/cherry-leaves)).\n"
+        f"It consists of 2104 images of healthy cherry leaves and 2104 images of leaves infected with powdery mildew,\n"
+        f"providing a balanced dataset for training and evaluation."
+    )
 
     st.success(
-        f"The project has 2 business requirements:\n"
-        f"* 1 - The client is interested in having a study to differentiate "
-        f"a parasitized and uninfected cell visually.\n"
-        f"* 2 - The client is interested in telling whether a given cell contains a malaria parasite or not. "
-        )
+        f"The project aims to address two key objectives:\n\n"
+        f"1. **Visual Differentiation:** Develop a method to visually distinguish healthy cherry leaves from those infected with powdery mildew.\n\n"
+        f"2. **Disease Classification:** Create a model capable of accurately classifying a given cherry leaf image as healthy or infected."
+    )
